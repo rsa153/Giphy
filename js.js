a30b58a1e76699c57d23234995b37005daacc99f
rsa153 <r-saber@nlaw.northwestern.edu>
2019-01-16 23:58:34 -0600

    update

--- js.js
+++ js.js
@@ -1,26 +1,45 @@
 var topics = ["monkey", "elephant", "sloth", "leopard"];
 
-function renderButtons() {
-    $("#button-view").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var a = $("<button>");
-        // Adding a class
-        a.addClass("animalClass");
-        // Adding a data-attribute with a value of the movie at index i
-        a.attr("data-name", topics[i]);
-        // Providing the button's text with a value of the movie at index i
-        a.text(topics[i]);
-        // Adding the button to the HTML
-        $("#button-view").append(a);
-    }
-}
+        function displayAnimal() {
+            var input = $(this).attr("data-name");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                input + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-// This function handles events where one button is clicked
-$("#add-animal").on("click", function (event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
-    renderButtons();
-});
-// Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+
+                .then(function (response) {
+                    console.log(queryURL);
+                    var results = response.data;
+                    for (var i = 0; i < results.length; i++) {
+                        var animalDiv = $("<div>");
+                        var p = $("<p>").text("Rating: " + results[i].rating);
+                        var animalImage = $("<img>");
+                        animalImage.attr("src", results[i].images.fixed_height_small_still.url);
+                        animalDiv.append(p);
+                        animalDiv.append(animalImage);
+                        $("#animal-view").prepend(animalDiv);
+                    }
+                });
+            }
+            function renderButtons() {
+                $("#button-view").empty();
+                for (var i = 0; i < topics.length; i++) {
+                    var a = $("<button>");
+                    a.addClass("animal-class");
+                    a.attr("data-name", topics[i]);
+                    a.text(topics[i]);
+                    $("#button-view").append(a);
+                }
+            }
+
+            $("#add-animal").on("click", function (event) {
+                event.preventDefault();
+                var input = $("#animal-input").val().trim();
+                topics.push(input);
+                renderButtons();
+            });
+            $(document).on("click", ".animal-class", displayAnimal);
+            renderButtons();
